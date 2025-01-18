5f6cacf7337e449d9bee3e5a5f81f523cee97555
Julianozz <zangrandizago@gmail.com>
2025-01-18 23:28:08 +0000

    criacao do super triunfo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -17,6 +17,41 @@ int main() {
     // Exibição dos Dados das Cartas:
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+    
+    
+    int cidade; 
+    int nome; 
+    float populacao;
+    float area;
+    int PIB;
+    int pontos;
+
+    printf("Desafio Super Triunfo Paises:\n");
+
+    printf ("Insira o código da cidade:\n");
+    scanf ("%d", &cidade);
+    fflush(stdin);
+    printf ("Insira o Nome:\n");
+    scanf ("%d", &nome);
+    fflush(stdin);
+    printf ("Insira a populacao:\n");
+    scanf ("%f", &populacao);
+    fflush(stdin);
+    printf ("Insira a area:\n");
+    scanf ("%f", &area);
+    fflush(stdin);
+    printf ("Insira o PIB:\n");
+    scanf ("%d", &PIB);
+    fflush(stdin);
+    printf ("Insira o ponto turistico:\n");
+    scanf ("%d", &pontos);
+
+    printf("codigo da cidade:%d\n",cidade);
+    printf("Nome:%d\n",nome);
+    printf("População:%f\n", populacao);
+    printf("área:%f\n", area);
+    printf("PIB:%d\n",PIB);
+    printf("ponto turistico:%d\n",pontos);
 
     return 0;
 }
