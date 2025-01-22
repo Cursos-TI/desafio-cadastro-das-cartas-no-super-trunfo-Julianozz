1599edbdd04ec1bdcb4dcdc3e0fdbbce67093978
Julianozz <zangrandizago@gmail.com>
2025-01-22 22:43:08 +0000

    fiz algumas alteracoes

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -18,38 +18,39 @@ int main() {
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
     
-    
+    // abaixo declaração de variaveis
     int cidade; 
     int nome; 
     float populacao;
     float area;
     int PIB;
     int pontos;
-
+    
+    // tema do meu jogo
     printf("Desafio Super Triunfo Paises:\n");
-
-    printf ("Insira o código da cidade:\n");
-    scanf ("%d", &cidade);
-    fflush(stdin);
-    printf ("Insira o Nome:\n");
-    scanf ("%d", &nome);
-    fflush(stdin);
-    printf ("Insira a populacao:\n");
-    scanf ("%f", &populacao);
-    fflush(stdin);
-    printf ("Insira a area:\n");
-    scanf ("%f", &area);
-    fflush(stdin);
-    printf ("Insira o PIB:\n");
-    scanf ("%d", &PIB);
-    fflush(stdin);
-    printf ("Insira o ponto turistico:\n");
-    scanf ("%d", &pontos);
-
+     
+    printf ("Insira o código da cidade:\n",cidade);//aqui imprime ao usuario uma msg de inserir o codigo
+    scanf ("%d", &cidade);// entrada do codigo feita pelo usuario
+    fflush(stdin);// limpar o baffer do teclado
+    printf ("Insira o Nome:\n",nome);// imprime a msg
+    scanf ("%d", &nome);// entrada 
+    fflush(stdin);// lipmpa o baffer 
+    printf ("Insira a populacao:\n",populacao);// imprime a msg
+    scanf ("%f", &populacao);// entrada 
+    fflush(stdin);// lipma
+    printf ("Insira a area:\n",area);// imprime
+    scanf ("%f", &area);// entrada
+    fflush(stdin); // limpa
+    printf ("Insira o PIB:\n",PIB);// imprime 
+    scanf ("%d", &PIB);// entrada 
+    fflush(stdin);// limpa
+    printf ("Insira o ponto turistico:\n",pontos);// imprima
+    scanf ("%d", &pontos); //entrada
+    // esta parte do codigo imprime tudo que o usuario digitou
     printf("codigo da cidade:%d\n",cidade);
     printf("Nome:%d\n",nome);
-    printf("População:%f\n", populacao);
-    printf("área:%f\n", area);
+    printf("Populacao:%.2f\n", populacao);
+    printf("area:%.2f\n", area);
     printf("PIB:%d\n",PIB);
     printf("ponto turistico:%d\n",pontos);
 
